82507f22fb1ab2786c5431f3a6615e9c15effb1f
Ge <ghschneid@gmail.com>
2022-05-21 07:50:38 +0200

    first test for line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+
+#[test]
+fn line_access() {
+    type Line = [i32; 10];
+    let mut line: Line = [0;10];
+    line [0] = 1;
+    assert_eq! (line [0], 1);
+}
+    
 fn main() {
     println!("Hello, world!");
 }
