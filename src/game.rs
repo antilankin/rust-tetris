9b6cf573f993741fe6cbe5bc5a233bca6ee678ae
Ge <ghschneid@gmail.com>
2022-05-26 21:46:28 +0200

    No magic number

--- src/game.rs
+++ src/game.rs
@@ -1,15 +1,19 @@
 use crate::board::{empty_board, Position};
 use crate::tetromino::{Orientation, Shape, Tetromino};
 
+fn start_position() -> Position {
+    [4, 22]
+}
+
 fn spawn(shape: Shape) -> (Tetromino, Position) {
-    (Tetromino::new(shape), [4, 22])
+    (Tetromino::new(shape), start_position())
 }
 
 #[test]
 fn test_spawn() {
     let board = empty_board();
     let (mut tetromino, position) = spawn(Shape::I);
-    assert_eq!(position, [4, 22]);
+    assert_eq!(position, start_position());
     for orientation in [
         Orientation::North,
         Orientation::East,
