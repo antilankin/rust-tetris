a4833a8cdb8610222c5ccde8983a2d6369bd76b9
Ge <ghschneid@gmail.com>
2022-06-05 23:15:01 +0200

    Test rotations

--- src/tetromino.rs
+++ src/tetromino.rs
@@ -1,3 +1,5 @@
+use std::path::Components;
+
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub enum Shape {
     I,
@@ -42,20 +44,75 @@ impl Tetromino {
     }
 
     pub fn blocks(&self) -> [[i32; 2]; 4] {
-        match self.shape {
-            Shape::I => match self.orientation {
-                Orientation::North => [[-1, 0], [0, 0], [1, 0], [2, 0]],
-                Orientation::East => [[1, 1], [1, 0], [1, -1], [1, -2]],
-                Orientation::South => [[-1, -1], [0, -1], [1, -1], [2, -1]],
-                Orientation::West => [[0, 1], [0, 0], [0, -1], [0, -2]],
-            },
-        }
+        tetromino_blocks(self.shape, self.orientation)
+    }
+}
+
+fn tetromino_blocks(shape: Shape, orientation: Orientation) -> [[i32; 2]; 4] {
+    match shape {
+        Shape::I => match orientation {
+            Orientation::North => [[-1, 0], [0, 0], [1, 0], [2, 0]],
+            Orientation::East => [[1, 1], [1, 0], [1, -1], [1, -2]],
+            Orientation::South => [[-1, -1], [0, -1], [1, -1], [2, -1]],
+            Orientation::West => [[0, 1], [0, 0], [0, -1], [0, -2]],
+        },
+    }
+}
+
+fn rotate_position_clockwise(position: [i32; 2], origin_scaled_by_two: [i32; 2]) -> [i32; 2] {
+    let x = position[0] * 2 - origin_scaled_by_two[0];
+    let y = position[1] * 2 - origin_scaled_by_two[1];
+    let x_t = (y + origin_scaled_by_two[0]) / 2;
+    let y_t = (-x + origin_scaled_by_two[1]) / 2;
+    [x_t, y_t]
+}
+
+fn rotate_positions_clockwise(
+    coordinates: [[i32; 2]; 4],
+    origin_scaled_by_two: [i32; 2],
+) -> [[i32; 2]; 4] {
+    let mut new_coordinates: [[i32; 2]; 4] = coordinates;
+    for (i, val) in coordinates.iter().enumerate() {
+        new_coordinates[i] = rotate_position_clockwise(*val, origin_scaled_by_two)
+    }
+    new_coordinates
+}
+
+fn origins_scaled_by_two(shape: Shape) -> [i32; 2] {
+    match shape {
+        Shape::I => [1, -1],
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_rotate_position_clockwise() {
+        assert_eq!(rotate_position_clockwise([1, 1], [0, 0]), [1, -1]);
+        assert_eq!(rotate_position_clockwise([1, -1], [0, 0]), [-1, -1]);
+        assert_eq!(rotate_position_clockwise([0, 0], [1, -1]), [1, 0]);
+        assert_eq!(rotate_position_clockwise([1, 0], [1, 1]), [0, 0]);
+
+        assert_eq!(
+            rotate_positions_clockwise([[-1, 0], [0, 0], [1, 0], [2, 0]], [1, -1]),
+            [[1, 1], [1, 0], [1, -1], [1, -2]]
+        );
+
+        assert_eq!(
+            rotate_positions_clockwise([[-1, 0], [0, 0], [0, 1], [1, 1]], [0, 0]),
+            [[0, 1], [0, 0], [1, 0], [1, -1]]
+        );
+
+        assert_eq!(
+            rotate_positions_clockwise(
+                tetromino_blocks(Shape::I, Orientation::North),
+                origins_scaled_by_two(Shape::I)
+            ),
+            tetromino_blocks(Shape::I, Orientation::East),
+        )
+    }
 
     #[test]
     fn test_rotate_tetromino() {
